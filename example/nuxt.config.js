1e53da06087620c146d14a2530750ca1c3b0f78e
franck.o.junior <fjunior.aragao@gmail.com>
2021-01-30 23:51:53 -0300

    fix(Lint): lint fix

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,14 +1,14 @@
-const { resolve } = require("path");
+const { resolve } = require('path')
 
 module.exports = {
-  rootDir: resolve(__dirname, ".."),
-  buildDir: resolve(__dirname, ".nuxt"),
+  rootDir: resolve(__dirname, '..'),
+  buildDir: resolve(__dirname, '.nuxt'),
   head: {
-    title: "nuxt-phometheus-module"
+    title: 'nuxt-phometheus-module'
   },
   srcDir: __dirname,
   render: {
     resourceHints: false
   },
-  modules: [{ handler: require("../") }]
-};
+  modules: [{ handler: require('../') }]
+}
